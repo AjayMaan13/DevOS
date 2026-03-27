aaf4f8a878a7aac9c021008555f5c31c4f013cec
AjayMaan13 <Ajaypsmaanm344@gmail.com>
2026-03-27 15:34:32 -0400

    added smoke test file and removed predefined files

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
